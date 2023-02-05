f2e57daf4d7f6f56dc3b0f4ce11ce4d196580db4
hhh131 <gmlrnjs131@naver.com>
2023-02-05 16:28:09 +0900

    Initial Commit

---
+++ AppleFramework/FrameworkDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FrameworkDetailViewController.swift
+//  AppleFramework
+//
+//  Created by 신희권 on 2023/02/05.
+//
+
+import UIKit
+import SafariServices
+
+class FrameworkDetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var framework : AppleFramework = AppleFramework(name: "Unknown", imageName: "", urlString: "", description: "")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        
+    }
+    func updateUI() {
+        imageView.image = UIImage(named: framework.imageName)
+        titleLabel.text = framework.name
+        descriptionLabel.text = framework.description
+    }
+
+    @IBAction func learnMoreTapped(_ sender: Any) {
+        guard let url = URL(string: framework.urlString) else {
+            return
+        }
+        
+        let safari = SFSafariViewController(url: url)
+        present(safari, animated: true)
+        
+    }
+    
+}
